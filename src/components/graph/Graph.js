823efad9fb12ff9611c2e8bbcfde9fc32aa0854d
lokendra <lokendrachaulagain803@gmail.com>
2022-04-12 09:20:19 +0545

    graph chart completed

--- src/components/graph/Graph.js
+++ src/components/graph/Graph.js
@@ -1,10 +1,50 @@
 import React from 'react'
-import "./graph.scss"	
+import "./graph.scss"
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+
+
+const data = [
+  { name: "January", Total: 1200 },
+  { name: "February", Total: 2100 },
+  { name: "March", Total: 800 },
+  { name: "April", Total: 1800 },
+  { name: "May", Total: 900 },
+  { name: "June", Total: 1500 },
+
+];
+
+
+
 
 function Graph() {
   return (
-    <div className='graphCon'>Graph</div>
+    <div className='graphCon'>
+      <span className="chartTitle">Last 6 months(income)</span>
+      <ResponsiveContainer className="actualChart" width="100%" height={320}>
+        <AreaChart width={730} height={250} data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+          <defs>
+            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+
+          </defs>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" className='chartColor'  />
+          <Tooltip />
+          <Area type="monotone" dataKey="Total" stroke="#8884d8" fillOpacity={1} fill="url(#total)" />
+
+        </AreaChart>
+      </ResponsiveContainer>
+
+
+
+
+    </div>
   )
 }
 
-export default Graph
+export default Graph 
